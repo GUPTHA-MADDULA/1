6b6f8c49a194ab265763db460e2c76d7f29dfced
Guptha <mrguptha5@gmail.com>
2025-01-30 12:34:37 +0530

    removed welcome message

--- clock.py
+++ clock.py
@@ -5,7 +5,7 @@ window.title("Clock")
 window.geometry("600x300")  # Corrected the geometry string
 window.configure(bg="steelblue")
 
-label = Label(window, text="Welcome", font=("Arial Black", 78, "bold"), bg="steelblue", fg="white")  # Fixed font syntax
+label = Label(window, font=("Arial Black", 78, "bold"), bg="steelblue", fg="white")  # Fixed font syntax
 label.pack(pady=100)
 
 window.mainloop()
